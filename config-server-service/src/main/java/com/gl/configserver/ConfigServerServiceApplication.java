ff39a570bad88e3a5710c9860b548a135a4cd4b2
yevhen.hryshchenko <yevhen.hryshchenko>
2018-11-27 12:09:38 +0200

    Add config server

---
+++ config-server-service/src/main/java/com/gl/configserver/ConfigServerServiceApplication.java
@@ -0,0 +1,14 @@
+package com.gl.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerServiceApplication.class, args);
+	}
+}
